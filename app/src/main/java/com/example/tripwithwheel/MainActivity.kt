f76117b280f6869ac5ff681203e01124e9dc085b
김이현 <rihyun1126@gmail.com>
2022-06-01 01:54:11 +0900

    하단 네비게이션 바 생성(페이지 이동 구현)

--- app/src/main/java/com/example/tripwithwheel/MainActivity.kt
+++ app/src/main/java/com/example/tripwithwheel/MainActivity.kt
@@ -2,10 +2,32 @@ package com.example.tripwithwheel
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.tripwithwheel.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
+
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.navigation.setOnItemSelectedListener { //하단 네비게이션 바에서 선택된 메뉴 확인
+            replaceFragment(
+                when (it.itemId) { //각 메뉴에 맞는 프래그먼트 지정
+                    R.id.menu_home -> HomeFragment()
+                    R.id.menu_list -> ListFragment()
+                    R.id.menu_schedule -> ScheduleFragment()
+                    else -> SettingsFragment()
+                }
+            )
+            true
+        }
+        binding.navigation.selectedItemId = R.id.menu_home
+    }
+
+    private fun replaceFragment(fragment : Fragment){ //지정된 프래그먼트로 현재 프래그먼트 대체
+        supportFragmentManager.beginTransaction().replace(R.id.content, fragment).commit()
     }
 }
