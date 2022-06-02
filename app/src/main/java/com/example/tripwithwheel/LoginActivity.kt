4c04198ae514a3fe75501a7ac7eeeea9683e311d
김이현 <rihyun1126@gmail.com>
2022-06-03 04:09:51 +0900

    로그인, 회원가입 페이지 화면에 뷰 배치 & 로그인 페이지를 초기 화면으로 하기 & 로그인 페이지에서 회원가입 페이지로 이동 구현

--- app/src/main/java/com/example/tripwithwheel/LoginActivity.kt
+++ app/src/main/java/com/example/tripwithwheel/LoginActivity.kt
@@ -1,11 +1,20 @@
 package com.example.tripwithwheel
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.tripwithwheel.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        //setContentView(R.layout.activity_login)
+        val binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.goToSignupBtn.setOnClickListener {
+            val intent = Intent(this, SignupActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
