de40ee1b7d9ef2399f2c1a6777bc252fb7231750
김이현 <rihyun1126@gmail.com>
2022-06-18 08:16:20 +0900

    Login 페이지 액션바 없애기 & Main 페이지 액션바 폰트 변경

--- app/src/main/java/com/example/tripwithwheel/SplashActivity.kt
+++ app/src/main/java/com/example/tripwithwheel/SplashActivity.kt
@@ -1,13 +1,16 @@
 package com.example.tripwithwheel
 
 import android.content.Intent
+import android.location.Geocoder
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.core.content.ContextCompat
 import java.util.concurrent.Executor
 import java.util.concurrent.Executors
 import java.util.concurrent.ScheduledExecutorService
 import java.util.concurrent.TimeUnit
+import kotlin.concurrent.thread
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,5 +29,17 @@ class SplashActivity : AppCompatActivity() {
                 finish()
             }
         }, 2, TimeUnit.SECONDS)
+    }
+
+    private fun geocoder(addr : String) : MutableList<Double> {
+        val geocoder = Geocoder(applicationContext)
+        val geocodedAddress = geocoder.getFromLocationName(addr, 1)
+
+        val latitude = geocodedAddress[0].latitude
+        val longitude = geocodedAddress[0].longitude
+
+        val loc = arrayListOf<Double>(latitude, longitude)
+
+        return loc
     }
 }
